74cfc502589761744b1505f28e17c91458082030
Rakhi-111 <85497155+Rakhi-111@users.noreply.github.com>
2021-09-18 19:31:52 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,12 +1,11 @@
-# add this file
 def add(x,y):
     pass
+
 def substract(x,y):
     pass
-#  multiply implementatio
+
 def multiply(x,y):
     pass
 
-#DEVIDE IMPLEMENTATION
 def dvide(x,y):
     pass
